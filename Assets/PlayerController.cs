bcf8cd8371e5ef3f76abd6af524bcd5b3a452615
tomcon634 <ulycc634@gmail.com>
2019-09-21 13:39:11 -0400

    Adding all bushes to level.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,15 +7,16 @@ public class PlayerController : MonoBehaviour
     public float movingSpeed = 20;
     public float rotationSpeed = 100;
 
-    int numFruit = 0;
+    int numFruits = 0;
 
     FruitBushScript fruitBushScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject fruitBush = GameObject.Find("FruitBush");
-        fruitBushScript = fruitBush.GetComponent<FruitBushScript>();
+        //fruitBushScript = GetComponent<FruitBushScript>();
+        //GameObject fruitBush = GameObject.Find("FruitBush");
+        //fruitBushScript = fruitBush.GetComponent<FruitBushScript>();
     }
 
     // Update is called once per frame
@@ -23,14 +24,19 @@ public class PlayerController : MonoBehaviour
     {
         transform.Translate(0f, 0f, movingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
         transform.Rotate(0, rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+
+        Debug.Log(numFruits);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "FruitBush")
         {
+            GameObject fruitBush = GameObject.Find(collision.transform.name);
+            fruitBushScript = fruitBush.GetComponent<FruitBushScript>();
+
             Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
-            numFruit += fruitBushScript.fruitsInBush;
+            numFruits += fruitBushScript.fruitsInBush;
             fruitBushScript.fruitsInBush = 0;
         }
     }
