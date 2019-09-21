6ec0dbc1d9a25de77f7a3999eb4187e33a8e67bb
David <davidw2009@hotmail.com>
2019-09-21 11:30:30 -0400

    added third person template

---
+++ Graden_Game/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movingSpeed = 100;
+    public float rotationSpeed = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f, 0f, movingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        transform.Rotate(0, rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+    }
+}
+
