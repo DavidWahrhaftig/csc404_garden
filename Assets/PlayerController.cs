e582153bf35d8d18ae81ffee548cc73fd5069962
tomcon634 <ulycc634@gmail.com>
2019-09-21 13:06:14 -0400

    Added walls and bush (along with prefabs).

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movingSpeed = 100;
+    public float movingSpeed = 20;
     public float rotationSpeed = 100;
+
+    int numFruit = 0;
+
+    FruitBushScript fruitBushScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject fruitBush = GameObject.Find("FruitBush");
+        fruitBushScript = fruitBush.GetComponent<FruitBushScript>();
     }
 
     // Update is called once per frame
@@ -17,6 +23,16 @@ public class PlayerController : MonoBehaviour
     {
         transform.Translate(0f, 0f, movingSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
         transform.Rotate(0, rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "FruitBush")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+            numFruit += fruitBushScript.fruitsInBush;
+            fruitBushScript.fruitsInBush = 0;
+        }
     }
 }
 
