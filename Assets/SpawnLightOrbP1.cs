ddee536de6ccd2c7f8d4a6bd7b4c45d6381d903f
pjsmith97 <48391420+pjsmith97@users.noreply.github.com>
2019-10-05 20:17:30 -0400

    Add files via upload
    Light orb scripts

---
+++ Assets/SpawnLightOrbP1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLightOrbP1 : MonoBehaviour
+{
+
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+    private GameObject effectToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectToSpawn = vfx[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w")) {
+            CreateEffect();
+        }
+    }
+
+    void CreateEffect()
+    {
+        GameObject visualEffect;
+
+        if (firePoint != null)
+        {
+            visualEffect = Instantiate(effectToSpawn, firePoint.transform.position,
+                Quaternion.identity);
+            visualEffect.transform.rotation = firePoint.transform.rotation;
+        }
+
+        else
+        {
+            Debug.Log("Null Fire Point");
+        }
+    }
+}
