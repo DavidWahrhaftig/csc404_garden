20cdd93f417a9ba295ffee9f43f28e8a9d4f5f2c
pjsmith97 <48391420+pjsmith97@users.noreply.github.com>
2019-10-07 02:02:23 -0400

    Add files via upload
    Script to attach to each player. When they collide with the specified enemy projectile, they turn yellow. Can add additional features related to witch AI at meet up

---
+++ Assets/LightUpTestScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightUpTestScript : MonoBehaviour
+{
+    public GameObject enemyProjectile;
+    private bool lightUp;
+    private float lightTime = 10;
+    private Color ogColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightUp)
+        {
+            lightTime -= Time.smoothDeltaTime;
+
+            if (lightTime < 0)
+            {
+                //Debug.Log("OG COLOR CHANGE!!!");
+                lightUp = false;
+                var playerRend = gameObject.GetComponent<Renderer>();
+                playerRend.material.SetColor("_Color", ogColor);
+                lightTime = 10;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == enemyProjectile.name + "(Clone)")
+        {
+            //Debug.Log("COLOR CHANGE!!!");
+            var playerRend = gameObject.GetComponent<Renderer>();
+            ogColor = playerRend.material.GetColor("_Color");
+            playerRend.material.SetColor("_Color", Color.yellow);
+            lightUp = true;
+        }
+
+        //else
+        //{
+        //    Debug.Log("Colliding Object: " + collision.gameObject.name);
+        //    Debug.Log("Want Object: " + enemyProjectile.name);
+        //}
+    }
+}
