1ca4899caac4c3d905c74de152609501d7fa6054
tomcon634 <ulycc634@gmail.com>
2019-10-07 15:52:12 -0400

    Adding light ball timer.

--- Assets/SpawnLightOrbP1.cs
+++ Assets/SpawnLightOrbP1.cs
@@ -8,6 +8,8 @@ public class SpawnLightOrbP1 : MonoBehaviour
     public GameObject firePoint;
     public List<GameObject> vfx = new List<GameObject>();
     private GameObject effectToSpawn;
+    private float spawnTime = 15;
+    private bool charge = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +19,25 @@ public class SpawnLightOrbP1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire3")) {
-            CreateEffect();
+        if (charge)
+        {
+            if (Input.GetButtonDown("Fire3"))
+            {
+                CreateEffect();
+                charge = false;
+            }
         }
+
+        else
+        {
+            spawnTime -= Time.smoothDeltaTime;
+            if (spawnTime < 0)
+            {
+                charge = true;
+                spawnTime = 15;
+            }
+        }
+        
     }
 
     void CreateEffect()
