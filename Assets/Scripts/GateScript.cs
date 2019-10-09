a5a049f730b01adee2bd4701ece4be92eae74ed6
Philip Smith <philipsmith@Philips-MacBook-Pro.local>
2019-10-09 01:51:53 -0400

    Gate now opens when players get close

---
+++ Assets/Scripts/GateScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+
+    Animator gateAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gateAnim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player1" || other.transform.tag == "Player2")
+        {
+            gateAnim.SetTrigger("OpenGate");
+        }
+            
+    }
+
+    private void HoldDoorOpen()
+    {
+        gateAnim.enabled = false;
+    }
+}
