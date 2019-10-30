f1fc4062731471c8341f17705633da3cb83b4d9f
David <davidw2009@hotmail.com>
2019-10-29 20:40:09 -0400

    Added fruit Oscillation and scaled UI componenets larger

--- Assets/Scripts/Witch Script/Oscillator.cs
+++ Assets/Scripts/Witch Script/Oscillator.cs
@@ -12,10 +12,13 @@ public class Oscillator : MonoBehaviour
     float movementFactor;
     Vector3 startingPos; // must be stored for absolute movement
 
+    private float random;
+
     // Start is called before the first frame update
     void Start()
     {
         startingPos = transform.position; // get initial position of gameObject from Transform componenet
+        random = Random.Range(0, 2);
     }
 
     // Update is called once per frame
@@ -25,9 +28,20 @@ public class Oscillator : MonoBehaviour
         if (period <= Mathf.Epsilon) { return; }
         float cycles = Time.time / period;
         const float tau = Mathf.PI * 2f; // about 6.28
-        float rawSinWave = Mathf.Sin(cycles * tau);
+        //float rawSinWave = Mathf.Sin(cycles * tau);
+        float rawWave;
 
-        movementFactor = rawSinWave / 2f + 0.5f;
+        if (random == 0)
+        {
+            rawWave = Mathf.Sin(cycles * tau);
+        }
+        else
+        {
+            rawWave = Mathf.Cos(cycles * tau);
+        }
+
+
+        movementFactor = rawWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         if (isOscillating)
         {
