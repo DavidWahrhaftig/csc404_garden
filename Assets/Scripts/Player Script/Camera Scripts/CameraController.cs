752bb9ead7d4f0378f70db057468e56d24cd4847
David <davidw2009@hotmail.com>
2019-11-01 22:21:24 -0400

    added camera shake feature when player is caught by witch, using buttons L1 and R1

--- Assets/Scripts/Player Script/Camera Scripts/CameraController.cs
+++ Assets/Scripts/Player Script/Camera Scripts/CameraController.cs
@@ -5,18 +5,17 @@ using Rewired;
 
 public class CameraController : MonoBehaviour
 {
-    private Quaternion originalRotation;
     public float rotationSpeed = 20f;
-    private Rewired.Player gamePadController;
-    float lookVertical;
 
-    bool cameraFlip;
+    // Game Pad related fields
+    private Rewired.Player gamePadController;
+    private float lookVertical;
+    private bool cameraFlip;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        originalRotation = transform.rotation;
-        //gamePadController = GetComponentInParent<PlayerController>().getGamePadController();
     }
 
     // Update is called once per frame
@@ -26,20 +25,20 @@ public class CameraController : MonoBehaviour
 
         lookVertical = gamePadController.GetAxis("Look Vertical");
         cameraFlip = gamePadController.GetButtonDown("Camera Flip");
+        
 
         transform.Rotate(rotationSpeed * lookVertical * Time.deltaTime, 0, 0); // camera vertical rotation
         
-
+        
         if (cameraFlip)
         {
             // move camera in front/back of player
-            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z * -1);
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z * -1f);
             
             // flip camera
             transform.Rotate(0f, 180, 0f, Space.World);
             
             // inverse the horizontal movment and horizontal rotation in PlayerController.cs
-        }
-               
+        }          
     }
 }
---
+++ Assets/Scripts/Player Script/Camera Scripts/CameraShake.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+
+    private Rewired.Player gamePadController;
+
+    private Animator camAnim;
+    private bool shakeLeft, shakeRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gamePadController = GetComponentInParent<PlayerController>().getGamePadController();
+        shakeLeft = gamePadController.GetButtonDown("ShakeLeft");
+        shakeRight = gamePadController.GetButtonDown("ShakeRight");
+
+        if (GetComponentInParent<PlayerLogic>().getIsCaught())
+        {
+            if (shakeLeft)
+            {
+                camShakeLeft();
+
+            }
+            else if (shakeRight)
+            {
+                camShakeRight();
+            }
+        }
+    }
+
+    public void camShakeLeft()
+    {
+        camAnim.SetTrigger("Shake Left");
+    }
+
+    public void camShakeRight()
+    {
+        camAnim.SetTrigger("Shake Right");
+    }
+}
