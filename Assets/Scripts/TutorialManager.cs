3dcbbb50e0f59a982feec9128298aab653925757
Lynn Liu <32230723+lynnlliu@users.noreply.github.com>
2019-11-11 21:24:46 -0500

    Update TutorialManager.cs

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,74 +1,140 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class TutorialManager : MonoBehaviour
 {
     public GameObject[] popUps;
     private int popUpIndex;
 
+    /***
+    #region Controller Inputs
+    float moveVertical;
+    float moveHorizontal;
+    float rotateHorizontal;
+    bool jumpButton;
+    bool runButton;
+    bool isCameraFlipped;
+    int flip = 1;
+    #endregion ***/
+
+    public Transform player1;
+    public Transform player2;
+    public Transform witch;
+
+
+
     private void Update()
     {
-        for (int i = 0; i < popUps.Length; i++) {
+        /***
+        moveVertical = gamePadController.GetAxis("Move Vertical");
+        moveHorizontal = gamePadController.GetAxis("Move Horizontal");
+        rotateHorizontal = gamePadController.GetAxis("Rotate");
+        jumpButton = gamePadController.GetButtonDown("Jump");
+        runButton = gamePadController.GetButton("Run");
+        isCameraFlipped = gamePadController.GetButtonDown("Camera Flip");
+        ****/
+
+        for (int i = 0; i < popUps.Length; i++)
+        {
             if (i == popUpIndex)
-                {
-                    popUps[popUpIndex].SetActive(true);
-                }
-                else
-                {
-                    popUps[popUpIndex].SetActive(false);
-                }
+            {
+                popUps[popUpIndex].SetActive(true);
+            }
+            else
+            {
+                popUps[popUpIndex].SetActive(false);
+            }
 
 
         }
-        if (popUpIndex == 0)
+        if (popUpIndex == 0) /**move**/
         {
-            if (false)/**left and right movement**/
+            bool player1Moved = false;
+            bool player2Moved = false;
+
+            if (player1.GetComponent<PlayerController>().getGamePadController().GetButtonDown("Move Vertical"))
+            {
+                player1Moved = true;
+            }
+
+            if (player2.GetComponent<PlayerController>().getGamePadController().GetButtonDown("Move Vertical"))
+            {
+                player2Moved = true;
+            }
+
+            if (player1Moved && player2Moved)/**both players have moved**/
             {
                 popUpIndex++;
             }
         }
 
-        else if (popUpIndex == 1)
+        else if (popUpIndex == 1) /***jump***/
         {
-            if (false)/**jump**/
+
+            bool player1Jumped = false;
+            bool player2Jumped = false;
+
+            if (player1.GetComponent<PlayerController>().getGamePadController().GetButtonDown("Move Vertical"))
+            {
+                player1Jumped = true;
+            }
+
+            if (player2.GetComponent<PlayerController>().getGamePadController().GetButtonDown("Move Vertical"))
+            {
+                player2Jumped = true;
+            }
+
+            if (player1Jumped && player2Jumped)/**both players have jumped**/
             {
                 popUpIndex++;
             }
         }
 
-        if (popUpIndex == 2)
+        else if (popUpIndex == 2) /**flip camera**/
         {
-            if (false)/**rotate camera**/
+            bool player1CamFlip = false;
+            bool player2CamFlip = false;
+
+            if (player1.GetComponent<PlayerController>().getGamePadController().GetButtonDown("Camera Flip"))
+            {
+                player1CamFlip = true;
+            }
+
+            if (player2.GetComponent<PlayerController>().getGamePadController().GetButtonDown("Camera Flip"))
+            {
+                player2CamFlip = true;
+            }
+
+            if (player1CamFlip && player2CamFlip)/**both players have flipped camera**/
             {
                 popUpIndex++;
             }
         }
 
-        if (popUpIndex == 3)
+        else if (popUpIndex == 3)
         {
-            if (false)/**flip camera**/ {
+            if (false)/**get fruit**/
+            {
                 popUpIndex++;
             }
         }
 
-        if (popUpIndex == 4)
+        else if (popUpIndex == 4)
         {
-            if (false)/**get fruit**/ {
+            if (false)/**throw light ball**/
+            {
                 popUpIndex++;
             }
         }
 
-        if (popUpIndex == 5)
+        else if (popUpIndex == 5)
         {
-            if (false)/**throw light ball**/ {
-                popUpIndex++;
-            }
-        }
-
-        if (popUpIndex == 6)
-        {
-            if (false)/**shake off the witch**/ {
+            if (false)/**shake off the witch**/
+            {
                 popUpIndex++;
             }
         }
