d42d202928fd2eb1c60a96c05c6e3cc28161bc4d
tomcon634 <ulycc634@gmail.com>
2019-11-20 16:39:53 -0500

    Added return to menu option in instrucitons scene.

--- Assets/Scripts/InstructionsManager.cs
+++ Assets/Scripts/InstructionsManager.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InstructionsManager : MonoBehaviour
 {
+    [SerializeField] Rewired.Player gamePadController;
+
     public Texture2D controlsImage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gamePadController = Rewired.ReInput.players.GetPlayer(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gamePadController.GetButtonDown("Jump"))
+            SceneManager.LoadScene("MainMenu2");
     }
 
     private void OnGUI()
